ec33b6c3dfa69ee7da79d8bc42a89e1e733f7f9c
Gerald Pape <gerald@giantswarm.io>
2025-01-30 14:17:48 +0100

    Allow channel selection for each individual CRD

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,8 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Allow selecting channel ("standard" or "experimental") for each individual CRD
+
+### Changed
+
 - Upgrade Gateway API CRDs to [v1.2.1](https://github.com/kubernetes-sigs/gateway-api/releases/tag/v1.2.1)
-- Switch to Gateway API Experimental channel
 
 ## [1.1.0] - 2024-05-13
 
--- sync/sync.sh
+++ sync/sync.sh
@@ -13,20 +13,22 @@ vendir sync
 { set +x; } 2>/dev/null
 
 # Patches
-./sync/patches/templates-helper/patch.sh
+./sync/patches/split-crds/patch.sh
 
-# Store diffs
-rm -f ./diffs/*
-for f in $(git --no-pager diff --no-exit-code --no-color --no-index vendor/gateway-api helm/gateway-api-crds/templates --name-only) ; do
-        [[ "$f" == "helm/gateway-api-crds/templates/_helpers.yaml" ]] && continue
-        set +e
-        set -x
-        git --no-pager diff --no-exit-code --no-color --no-index "vendor/gateway-api/${f#"helm/gateway-api-crds/templates/"}" "${f}" \
-                > "./diffs/${f//\//__}.patch" # ${f//\//__} replaces all "/" with "__"
-        ret=$?
-        { set +x; } 2>/dev/null
-        set -e
-        if [ $ret -ne 0 ] && [ $ret -ne 1 ] ; then
-                exit $ret
-        fi
-done
+# Skip diff storage for now
+
+## Store diffs
+#rm -f ./diffs/*
+#for f in $(git --no-pager diff --no-exit-code --no-color --no-index vendor/gateway-api helm/gateway-api-crds/templates --name-only) ; do
+#        [[ "$f" == "helm/gateway-api-crds/templates/_helpers.yaml" ]] && continue
+#        set +e
+#        set -x
+#        git --no-pager diff --no-exit-code --no-color --no-index "vendor/gateway-api/${f#"helm/gateway-api-crds/templates/"}" "${f}" \
+#                > "./diffs/${f//\//__}.patch" # ${f//\//__} replaces all "/" with "__"
+#        ret=$?
+#        { set +x; } 2>/dev/null
+#        set -e
+#        if [ $ret -ne 0 ] && [ $ret -ne 1 ] ; then
+#                exit $ret
+#        fi
+#done

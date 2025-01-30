96d0ff185abb16e76b13450ece984b89cf48c08b
Gerald Pape <ubergesundheit@users.noreply.github.com>
2025-01-30 15:20:17 +0100

    Bump gateway api to 1.2.1 + add sync wrapper (#51)
    * Bump gateway api to 1.2.1 + add sync wrapper
    
    * Switch to experimental channel
    
    * Allow channel selection for each individual CRD
    
    * Add comments to values

---
+++ sync/patches/split-crds/patch.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+repo_dir=$(git rev-parse --show-toplevel) ; readonly repo_dir
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly script_dir
+
+cd "${repo_dir}"
+
+raw_crds_path="../../../vendor/gateway-api"
+templates_path="./helm/gateway-api-crds/templates/"
+
+set -x
+
+cd "${templates_path}"
+yq -s '.metadata.annotations."gateway.networking.k8s.io/channel" + "-" + .metadata.name + ".yaml"' "${raw_crds_path}"/*.yaml
+
+{ set +x; } 2>/dev/null
+for f in *.yaml ; do
+  [[ "$f" == "_helpers.yaml" ]] && continue
+
+  name=$(yq '.spec.names.plural' $f)
+  channel=$(yq '.metadata.annotations."gateway.networking.k8s.io/channel"' $f)
+  set -x
+  echo "{{ if eq .Values.install.$name \"$channel\" }}" | cat - $f > _temp #
+  echo "{{ end }}" >> _temp
+  mv _temp $f
+
+  { set +x; } 2>/dev/null
+done
+
+set -x
+cd "${repo_dir}"
+{ set +x; } 2>/dev/null
